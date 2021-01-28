e964d8671fba6cda69fdf2c90048b541dcbd737e
Ricky Amparo <rickyamparo@gmail.com>
2021-01-27 17:17:50 -0700

    Add test for checking that application waits 5 seconds

--- post_test.py
+++ post_test.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 import json
+import time
 
 @pytest.fixture
 def base_url():
@@ -10,7 +11,10 @@ def test_valid_post_hash(base_url):
     url = base_url + "/hash"
     data = {'password':'angrymonkey'}
     headers = {'Accept':'application/json'}
+    begin = time.time()
     response = requests.post(url, json=data, headers=headers)
+    end = time.time()
     hash_id = json.loads(response.text)
     assert response.status_code == 200
-    assert isinstance(hash_id, str)
+    assert round(end - begin) == 5
+    assert isinstance(hash_id, int)
