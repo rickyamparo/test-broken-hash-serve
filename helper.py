5154f6cff956cecb56ee55894ba5b50b3fe26de4
Ricky Amparo <rickyamparo@gmail.com>
2021-01-29 15:03:26 -0700

    Add json checker funtion to helper

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import json
 
 class Helper:
     def _init_(self):
@@ -10,3 +11,10 @@ class Helper:
     
     def base_url(self):
         return "http://127.0.0.1:" + str(os.environ['PORT'])
+
+    def is_json(self, jsonobject):
+        try:
+            json.loads(jsonobject)
+        except ValueError:
+            return False
+        return True
