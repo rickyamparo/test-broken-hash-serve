7a201632d4f52cbee7b1f347e28dc26a00e1f21d
Ricky Amparo <rickyamparo@gmail.com>
2021-01-29 15:36:33 -0700

    Removed helper functions from test

--- test_get_hash.py
+++ test_get_hash.py
@@ -2,29 +2,15 @@ import pytest
 import requests
 import json
 import base64
-import os
 import time
+from helper import Helper
 
-@pytest.fixture
-def base_url():
-    return "http://127.0.0.1:" + str(os.environ['PORT'])
+test_helper = Helper()
 
-def create_hash_id(base_url):
-    data = {'password':'angrymonkey'}
-    headers = {'Accept':'application/json'}
-    response = requests.post(base_url + "/hash", json=data, headers=headers)
-    return response.text
-
-def isBase64(s):
-    try:
-        return base64.b64encode(base64.b64decode(s)) == s
-    except Exception:
-        return False
-
-def test_valid_get_hash(base_url):
-    hash_id = create_hash_id(base_url)
-    url = base_url + "/hash/" + str(hash_id)
+def test_valid_get_hash():
+    hash_id = test_helper.create_hash_id()
+    url = test_helper.base_url() + "/hash/" + str(hash_id)
     headers = {'Accept':'application/json'}
     response = requests.get(url, headers=headers)
     assert response.status_code == 200
-    assert(isBase64(response.text))
+    assert(test_helper.isBase64(response.text))
