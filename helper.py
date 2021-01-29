3bf3fdf69f91168f6c89105df5449ca74b4d375b
Ricky Amparo <rickyamparo@gmail.com>
2021-01-29 14:22:56 -0700

    Create Helper calss to house repated functions

---
+++ helper.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+
+class Helper:
+    def _init_(self):
+        pass
+
+    def open_app(self):
+        return subprocess.Popen(['open', str(os.environ['BROKEN_HASH'])])
+
+    
